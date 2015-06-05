29d155b28ade113f0d902665c7f3c446127eb048
roshando <allan.sequeira@gmail.com>
2015-06-05 00:23:24 -0500

    - added PlayerShot script - attached PlayerShot script and MoveScript with updated direction to PlayerShot game object - created PlayerShot prefab

---
+++ Assets/Scripts/ShotScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Projectile behavior
+/// </summary>
+public class ShotScript : MonoBehaviour {
+
+	// Designer variables
+
+	/// <summary>
+	/// Damage inflicted
+	/// </summary>
+	public int damage = 1;
+
+	/// <summary>
+	/// Projectile damage player or enemies
+	/// </summary>
+	public bool isEnemyShot = false;
+
+	// Use this for initialization
+	void Start () {
+		// Destroy game object after 20 seconds to avoid leak
+		Destroy (gameObject, 20);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
