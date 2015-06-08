44e0d3452248a899f4b6b7690f7a37e34e7257c3
roshando <allan.sequeira@gmail.com>
2015-06-07 19:16:42 -0500

    - added enemy shot image - duplicated playershot game object, renamed it to EnemyShot1, configured EnemyShot1 to use enemy shot sprite/image - Saved EnemyShot1 game object as a prefab - Attached weapon script to Enemy game object - Created EnemyScript and attached it to Enemy (poulpi) game object - Configured weapon Script shooting rate for Enemy game object to 0.75 - Deleted EnemyShot1 game object from Hierarchy/Scene

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Enemy generic behavior
+/// A simple behavior - trigger the weapon at each frame. A kind of auto-fire
+/// </summary>
+public class EnemyScript : MonoBehaviour {
+
+	private WeaponScript weapon;
+
+	void Awake() {
+		// Retrieve the weapon only once
+		weapon = GetComponent<WeaponScript> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// auto-fire
+		if (weapon != null && weapon.CanAttack) {
+			weapon.Attack(true);
+		}
+	}
+}
