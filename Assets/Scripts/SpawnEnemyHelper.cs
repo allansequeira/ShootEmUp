76995d1c2af8150e7dedcc715b7ef485b64b0c09
roshando <allan.sequeira@gmail.com>
2015-06-30 16:22:23 -0500

    - added SpawnEnemyHelper singleton skeleton to handle spawning enemies

---
+++ Assets/Scripts/SpawnEnemyHelper.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Helper class to spawn enemies
+/// </summary>
+public class SpawnEnemyHelper : MonoBehaviour {
+
+	/// <summary>
+	/// Singleton instance.
+	/// Access from everywhere using SpawnEnemyHelper.Instance
+	/// </summary>
+	public static SpawnEnemyHelper Instance;
+
+	/// <summary>
+	/// Enemy prefab
+	/// </summary>
+	public Transform enemyPreFab;
+
+	void Awake() {
+		// Register the singleton
+		if (Instance != null) {
+			Debug.LogError("Multiple instances of SpawnEnemyHelper");
+		}
+		
+		Instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
