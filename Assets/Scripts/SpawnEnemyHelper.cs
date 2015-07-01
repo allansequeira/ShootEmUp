138c8fa46c1cc5fca985f0f9bd95a6630579a462
roshando <allan.sequeira@gmail.com>
2015-06-30 23:59:07 -0500

    - created SpawnEnemyHelper singleton that exposes public APIs that can be called to spawn enemies - hardcoded the position where the enemy is spawned in the public API - added TODO to fix the above hard-coding

--- Assets/Scripts/SpawnEnemyHelper.cs
+++ Assets/Scripts/SpawnEnemyHelper.cs
@@ -25,6 +25,38 @@ public class SpawnEnemyHelper : MonoBehaviour {
 		
 		Instance = this;
 	}
+	
+	// TODO: Add randomness to x and y position when spawning enemies.
+	/// <summary>
+	/// Spawns the enemy at the given position
+	/// </summary>
+	/// <param name="position">Position.</param>
+	public void SpawnEnemy(Vector3 position) {
+		Vector3 pos = new Vector3(position.x + 20, position.y + 1, position.z);
+		Debug.Log ("Instantiating enemy at pos: " + pos);
+		instantiate (enemyPreFab, pos);
+	}
+
+	/// <summary>
+	/// Spawn the specified prefab at the given position.
+	/// </summary>
+	/// <param name="prefab">Prefab.</param>
+	/// <param name="position">Position.</param>
+	public void Spawn(Transform prefab, Vector3 position) {
+
+	}
+
+	/// <summary>
+	/// Instantiate the specified prefab at the specified position.
+	/// </summary>
+	/// <param name="prefab">Prefab.</param>
+	/// <param name="position">Position.</param>
+	/// <returns></returns>
+	private void instantiate(Transform prefab, Vector3 position) {
+		var enemy = Instantiate (prefab, 
+		                               position, Quaternion.identity) as Transform;
+		//return enemy;
+	}
 
 	// Use this for initialization
 	void Start () {
